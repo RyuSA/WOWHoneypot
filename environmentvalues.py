701456a1d1f17be48df7ff02928f9d2527100237
ryusa <kommsussertad@gmail.com>
2020-11-01 13:12:17 +0900

    環境変数ロードを実行するクラスを作成

---
+++ environmentvalues.py
@@ -0,0 +1,49 @@
+import os
+
+
+class EnvironmentValues:
+
+    def __init__(self) -> None:
+        # default port: 8080
+        self.port = 44333
+
+        # default server header: Apache
+        self.server_header = "Apache"
+
+        # art directory path
+        self.art_path = "./art/"
+
+        # WOWHoneypot logfile path
+        self.log_path = "./log/"
+
+        # Access log separator
+        self.separator = " "
+
+        # WOWHoneypot logfile name
+        self.wowhoneypot_log = "wowhoneypot.log"
+
+        # Syslog (Output facility: local0(16), priority: info, only tcp protocol)
+        self.syslog_enable = False
+        self.syslog_server = "127.0.0.1"
+        self.syslog_port = "514"
+
+        # Hunting
+        self.hunt_enable = False
+        self.hunt_log = "hunting.log"
+
+        # for GDPR(True: replace source ip address with 0.0.0.0)
+        self.ipmasking = False
+
+        # SSL certfile path
+        self.certfile_path = "./server.pem"
+
+        # default host header port: 80
+        self.host_port = 443
+
+    def loadEnv():
+        instance = EnvironmentValues()
+        for field in instance.__dict__.keys():
+            value = os.environ.get(field.upper())
+            if value:
+                exec("instance.{0} = {1}".format(field, value))
+        return instance
